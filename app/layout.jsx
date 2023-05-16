cca420a321eb22147fb71221e3148a16a21a2f66
ChrisKnowsDev <chrisharley81@gmail.com>
2023-05-16 19:34:19 -0400

    successfully posting prompt to DB on submit

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,14 +5,16 @@ import Provider from '@components/provider'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+      <body suppressHydrationWarning={true}>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
--- components/provider.jsx
+++ components/provider.jsx
@@ -1,3 +1,11 @@
-export default function Provider() {
-  return <div>Provider</div>
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+
+export default function Provider({ children, session }) {
+  return (
+    <div>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </div>
+  )
 }
