561f2fa38fb60e3b2df82715849e6e0aec75b8a8
ChrisKnowsDev <chrisharley81@gmail.com>
2023-05-17 08:43:25 -0400

    fetching all prompts and displaying promptCard component on home page

--- components/feed.jsx
+++ components/feed.jsx
@@ -1,3 +1,50 @@
+'use client'
+import { useState, useEffect } from 'react'
+import PromptCard from './prompt-card'
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map(post => (
+        <PromptCard
+          key={post.prompt}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
 export default function Feed() {
-  return <div>Feed</div>
+  const [searchText, setSearchText] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = e => {}
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleToggleClick={() => {}} />
+    </section>
+  )
 }
